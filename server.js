1113153c3f4c93eda9fbf8f93aa0d02dad436af1
Max Ly <lypc925@gmail.com>
2025-09-08 10:05:25 -0400

    fix server.js

--- server.js
+++ server.js
@@ -23,6 +23,7 @@ mongoose.connect(process.env.MONGO_URI || "mongodb://127.0.0.1:27017/blog-page")
 
 
 const UserSchema = new mongoose.Schema({
+    name: { type: String, required: true },
     username: { type: String, required: true, unique: true },
     password: { type: String, required: true }
 });
