d8bfb49ae4b7491946e2a5d1a53cca9260dd9f06
Max Ly <lypc925@gmail.com>
2025-09-14 16:24:54 -0400

    .

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -5,7 +5,7 @@ const User = require('../models/User')
 exports.getUser = async (req, res) => {
     try {
         const { userId } = req.params;
-        const user = User.findById(userId)
+        const user = await User.findById(userId)
         res.json(user)
     } catch (error) {
         res.status(500).json({ error: error.message })
