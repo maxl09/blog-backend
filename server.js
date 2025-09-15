e04fa829a68bb7d560412e95bdc056242cd73b33
Max Ly <lypc925@gmail.com>
2025-09-15 19:20:28 -0400

    .

--- server.js
+++ server.js
@@ -22,10 +22,7 @@ const PORT = process.env.PORT || 5002;
 const MONGO_URI = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/blog-page";
 // const MONGO_URI = "mongodb://127.0.0.1:27017/blog-page";
 
-mongoose.connect(MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
+mongoose.connect(MONGO_URI)
     .then(() => console.log('MongoDB connected to', mongoose.connection.name))
     .catch(err => console.error("MongoDB connection error:", err));
 
