a50bdf1d024fe1fe94f766267995c7a78269ae19
Max Ly <lypc925@gmail.com>
2025-09-19 22:17:40 -0400

    add create comment to post

--- src/controllers/commentController.js
+++ src/controllers/commentController.js
@@ -3,12 +3,12 @@ const Post = require('../models/Post')
 
 exports.createComment = async (req, res) => {
     try {
-        const { text } = req.body;
-        const { postId } = req.params;
+        const { text, postId } = req.body;
+        // const { postId } = req.params;
         const userId = req.user.id;
 
         const comment = await Comment.create({ text, author: userId, post: postId })
-        await Post.findByIdAndDelete(postId, { $push: { comments: comment._id } })
+        await Post.findByIdAndUpdate(postId, { $push: { comments: comment._id } }).sort({ createdAt: -1 })
 
         res.status(201).json(comment)
     } catch (error) {
