b64d28e83dcafd709c0046486e5d22c9bb523581
Max Ly <lypc925@gmail.com>
2025-09-23 20:49:16 -0400

    .

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ try {
     app.use(express.json());
     app.use(cors({
         origin: [
-            "http://localhost:5174",
+            "http://localhost:5173",
             "https://blog-frontend-4cy2.onrender.com"
         ],
         methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
